a48d05f9a36c56e88968832b9bceb77db272232a
empty herring <tom.still@live.co.uk>
2017-04-11 20:25:41 +0000

    Added ajax functionality

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,18 +1,31 @@
 /* global $ */
+/* global show_spinner */
+/* global hide_spinner */
 var init_stock_lookup;
 
 init_stock_lookup = function()
 {
+    $("#stock-lookup-form").on("ajax:before", function(event, data, status)
+    {
+        show_spinner();
+    });
+    $("#stock-lookup-form").on("ajax:after", function(event, data, status)
+    {
+        hide_spinner();
+    });
+    
+    
     $("#stock-lookup-form").on("ajax:success", function(event, data, status)
     {
         $("#stock-lookup").replaceWith(data);
         init_stock_lookup();
     });
     
-    $("#stock-lookup-form").on("ajax:error", function(event, xhr, status, error)
+    $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error)
     {
-        $("#stock-lookup-results").replaceWith(" ");
-        $("stock-lookup-errors").replaceWith("Stock was not found.");
+        hide_spinner();
+        $('#stock-lookup-results').replaceWith(' ');
+        $('#stock-lookup-errors').replaceWith('Stock was not found.');
     });
 }
 
